3165dbab0007eeeeb06f0cdd8ce899614442f017
bestdevelope <leabouillot7@gmail.com>
2024-05-30 17:53:01 +0200

    save ancien to do dans local storage

--- script/todo.js
+++ script/todo.js
@@ -2,16 +2,15 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.getElementById("todo-input");
 const toDoList = document.getElementById("todo-list");
 
-const toDo = []; //un tableau toDo
+let toDoArray = []; //un tableau toDo
 function saveToDos() {
-    localStorage.setItem("todos", toDo); //save toDo dans le local storage
+    localStorage.setItem("todos", JSON.stringify(toDoArray)); //save toDoArray dans le local storage le format JSON
 }
-
+//The JSON.parse() static method parses a JSON string, constructing the JavaScript value or object described by the string
 function deleteTodo(event) {
     const li = event.target.parentNode; //chaque button a different de parentNode, on peut effcer la liste qu on click
     li.remove();
 }
-
 
 function addTodo(newTodo) {
   const li = document.createElement("li");
@@ -23,14 +22,21 @@ function addTodo(newTodo) {
   toDoList.appendChild(li);
   li.appendChild(span);
   li.appendChild(button);
-  
 }
+
 function addTodoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = " ";
-  toDo.push(newTodo); //ajouter (push) dans un tableau toDo
+  toDoArray.push(newTodo); //ajouter (push) dans un tableau toDo
   addTodo(newTodo);
   saveToDos();//save toDo dans le local storage
 }
 toDoForm.addEventListener("submit", addTodoSubmit);
+
+const toDos = JSON.parse(localStorage.getItem("todos"));
+
+if (toDos) {
+  toDoArray = toDos; // ancien todos est dans le toDosArray (local storage)
+  toDoArray.forEach(addTodo);
+}
