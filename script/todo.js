24ac08759aebea892a1cfbcc5d58ba8dcb11759b
bestdevelope <leabouillot7@gmail.com>
2024-05-30 16:52:40 +0200

    local storage saveToDos();

--- script/todo.js
+++ script/todo.js
@@ -2,6 +2,11 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.getElementById("todo-input");
 const toDoList = document.getElementById("todo-list");
 
+const toDo = []; //un tableau toDo
+function saveToDos() {
+    localStorage.setItem("todos", toDo); //save toDo dans le local storage
+}
+
 function deleteTodo(event) {
     const li = event.target.parentNode; //chaque button a different de parentNode, on peut effcer la liste qu on click
     li.remove();
@@ -24,6 +29,8 @@ function addTodoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = " ";
+  toDo.push(newTodo); //ajouter (push) dans un tableau toDo
   addTodo(newTodo);
+  saveToDos();//save toDo dans le local storage
 }
 toDoForm.addEventListener("submit", addTodoSubmit);
