88600c1f77b9ecbfbbf29d6a57fdd01f08eb691f
bestdevelope <leabouillot7@gmail.com>
2024-05-30 16:04:57 +0200

    delete to do

--- script/todo.js
+++ script/todo.js
@@ -2,12 +2,23 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.getElementById("todo-input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteTodo(event) {
+    const li = event.target.parentNode; //chaque button a different de parentNode, on peut effcer la liste qu on click
+    li.remove();
+}
+
+
 function addTodo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo)
   toDoList.appendChild(li);
   li.appendChild(span);
-  span.innerText = newTodo;
+  li.appendChild(button);
+  
 }
 function addTodoSubmit(event) {
   event.preventDefault();
